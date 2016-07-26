e8fa4bec5f28201aebb73cf6ec1ab910aca3c363
Jari-Pekka Teurajärvi <teurajarvi@hotmail.com>
2016-07-26 13:31:16 +0300

    Create laskin.h
    Initial .h header file for calculator / laskin

---
+++ laskin.h
@@ -0,0 +1,13 @@
+/* laskin.h */
+
+/* variables */
+double luku_lopputulos = 0;
+
+#define LUKUVIRHE 9999
+
+/* funktions */
+void tarkistus( );
+double laske_yhteen( );
+double tee_vahennyslasku();
+double tee_kertolasku();
+double tee_jakolasku();
